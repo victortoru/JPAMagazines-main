2dbb32173fab47a8eb504aa184247db7aedb8c1c
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-03-13 17:58:03 +0100

    seguoir pasando del authors del model al coches.java

--- src/main/java/model/Coches.java
+++ src/main/java/model/Coches.java
@@ -22,4 +22,43 @@ public class Coches implements Serializable{
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "id_coche")
     public Coches coches;
+
+    public Coches(String carroceria, String combustible, int plazas,
+                   int precio) {
+        super();
+        this.carroceria = carroceria;
+        this.combustible = combustible;
+        this.plazas = plazas;
+        this.precio = precio;
+    }
+
+    public Coches() {
+        super();
+    }
+
+    public String getCarroceria() {
+        return carroceria;
+    }
+
+    public void setCarroceria(String carroceria) {
+        this.carroceria = carroceria;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public int getPlazas() {
+        return plazas;
+    }
+
+    public void setPlazas(int plazas) {
+        this.plazas = plazas;
+    }
+
+
 }
